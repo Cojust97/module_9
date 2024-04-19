cb8675071fa6b841dbc2351b1ddf2bbd904d64fd
Andreeva Svetlana <cojust@mail.ru>
2024-04-19 22:59:23 +0500

    k

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const btn = document.getElementById('content__button-load');
 const mask = document.getElementById('mask');
 btn.addEventListener('click', async () => {
-  mask.style.display = 'flex'; 
-  const response = await fetch('https://dog.ceo/api/breeds/image/random/20');
-  const data = await response.json();
-  const images = data.message;
+  mask.style.display = 'flex';
+  try {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random/20');
+    const data = await response.json();
+    const images = data.message;
 
-  const gallery = document.querySelector('.content__images');
-  gallery.innerHTML = '';
+    const gallery = document.querySelector('.content__images');
+    gallery.innerHTML = '';
 
-  images.forEach(imageUrl => {
-    const img = document.createElement('img');
-    img.src = imageUrl;
-    img.classList.add('image');
-    gallery.appendChild(img);
-}); 
-   mask.style.display = 'none';
+    images.forEach(imageUrl => {
+      const img = document.createElement('img');
+      img.src = imageUrl;
+      img.classList.add('image');
+      gallery.appendChild(img);
+    });
+  } catch (error) {
+    console.error('Произошла ошибка при загрузке изображений:', error);
+  } finally {
+    mask.style.display = 'none';
+  }
 });
